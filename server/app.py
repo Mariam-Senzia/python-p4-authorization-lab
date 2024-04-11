b44d9667159b42fb730de1c2dac90f5839e438f6
Mariam Senzia <mariamsenzia@gmail.com>
2024-04-11 23:04:35 +0300

    authorization

--- server/app.py
+++ server/app.py
@@ -83,16 +83,36 @@ class CheckSession(Resource):
             return user.to_dict(), 200
         
         return {}, 401
+    
+
+# @app.before_request
+# def check_if_logged_in():
+#     if not session.get('user_id'):
+#         return {'error': 'Unauthorized'}, 401
 
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+
+        articles = Article.query.filter(Article.is_member_only == True).all()
+
+        return [article.to_dict() for article in articles]
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized'}, 401
+
+        article = Article.query.filter(Article.id == id, Article.is_member_only == True).first()
+
+        if not article:
+            return {'message': 'Article not found'}, 404
+
+
+        return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
